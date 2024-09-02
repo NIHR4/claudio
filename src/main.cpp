19c6cfd92092b0a66d645939a3b90d2db6d2ca94
Andre Schiaffino <31828626+AndreNIH@users.noreply.github.com>
2024-09-01 20:10:41 -0700

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+
+
+
+
+int main(int, char**){
+   #ifdef _DEBUG
+    std::cout << "Fuckyyouuuuuuuuuuuuuuu!!!!";
+   #endif
+    std::cout << "Hello, from clarion-pp!\n";
+}
